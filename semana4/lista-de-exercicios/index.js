b52d8b162b88dca7a5f9f4b310d72f6335762967
WBernardo <souzawermeson@gmail.com>
2020-12-11 15:15:55 -0300

    Resolucao do exercício 2 - Interpretação

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,12 @@
+//Exercícios de interpretação de código
+
+/*Exercício 1: O código deste exercício mostra um função que solicita a cotação do dólar e, de acordo com
+a quantidade de dinheiro que o usuário tem em dólares, o retorna a quantidade que seria em real brasileiro.*/
+
+/*Exercício 2: Esta função solicita um tipo de investimento e calcula o rendimento do mesmo após sua aplicação.
+Especificamente nesta simulação, há uma variação de 2 investimentos com valores distintos, que resultarão
+nos seguintes dados:
+
+Ações ----> novoMontante ----> (165)
+Tesouro Direto ----> segundoMontante ----> (TIPO DE INVESTIMENTO INFORMADO INCORRETO!)
+*/
